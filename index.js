4014762dc10ca22a1209f913c1f08e162984cb99
yaroslavvyhivskyi <iarick.vigasi@gmail.com>
2017-11-27 17:05:25 +0200

    omx player with node js

---
+++ index.js
@@ -0,0 +1,73 @@
+var keypress = require('keypress');
+var OmxManager = require('omx-manager');
+var manager = new OmxManager(); // OmxManager
+// var camera = manager.create('video.avi'); // OmxInstance
+// camera.play(); // Will start the process to play videos
+
+var indexCamera = playIndex()
+
+// Play initial
+function playIndex() {
+  manager.enableNativeLoop();
+  var camera = manager.create('index_rotate.mov', {'--loop': true}); // OmxInstance
+  camera.play(); // Will start the process to play videos
+  return camera;
+}
+
+function play1789() {
+  indexCamera.stop();
+  var camera = manager.create('1789_rotate.mov');
+  camera.play();
+  return camera;
+}
+
+function play1850() {
+  indexCamera.stop();
+  var camera = manager.create('1850_rotate.mov');
+  camera.play();
+  return camera;
+}
+
+function play1900() {
+  indexCamera.stop();
+  var camera = manager.create('1900_rotate.mov');
+  camera.play();
+  return camera;
+}
+
+function play1950() {
+  indexCamera.stop();
+  var camera = manager.create('1900_rotate.mov');
+  camera.play();
+  return camera;
+}
+
+// make `process.stdin` begin emitting "keypress" events
+keypress(process.stdin);
+// listen for the "keypress" event
+process.stdin.on('keypress', function (ch, key) {
+  if (key.keyCode == 78) {
+    var camera = play1789()
+    camera.on('end', playIndex);
+  }
+  else if (key.keyCode == 77) {
+    var camera = play1850()
+    camera.on('end', playIndex);
+  }
+  else if (key.keyCode == 188) {
+    var camera = play1900()
+    camera.on('end', playIndex);
+  }
+  else if (key.keyCode == 190) {
+    var camera = play1950()
+    camera.on('end', playIndex);
+  }
+
+  console.log('got "keypress"', key);
+  if (key && key.ctrl && key.name == 'c') {
+    process.stdin.pause();
+  }
+});
+
+process.stdin.setRawMode(true);
+process.stdin.resume();
